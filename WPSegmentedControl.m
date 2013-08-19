bff7716d53f748df71bd47d655f7acd98e34bc50
panda <ody344@gmail.com>
2013-08-19 17:59:42 +0400

    fixes

--- WPSegmentedControl.m
+++ WPSegmentedControl.m
@@ -23,27 +23,27 @@
     if (self) {
         
         // Set divider images
-        [self setDividerImage:[UIImage imageNamed:@"segmented_control-divider-none-selected.png"]
+        [self setDividerImage:[UIImage imageNamed:@"segmented_control-divider-none-selected"]
           forLeftSegmentState:UIControlStateNormal
             rightSegmentState:UIControlStateNormal
                    barMetrics:UIBarMetricsDefault];
         
-        [self setDividerImage:[UIImage imageNamed:@"segmented_control-divider-left-selected.png"]
+        [self setDividerImage:[UIImage imageNamed:@"segmented_control-divider-left-selected"]
           forLeftSegmentState:UIControlStateSelected
             rightSegmentState:UIControlStateNormal
                    barMetrics:UIBarMetricsDefault];
         
-        [self setDividerImage:[UIImage imageNamed:@"segmented_control-divider-right-selected.png"]
+        [self setDividerImage:[UIImage imageNamed:@"segmented_control-divider-right-selected"]
           forLeftSegmentState:UIControlStateNormal
             rightSegmentState:UIControlStateSelected
                    barMetrics:UIBarMetricsDefault];
         
-        UIImage *normalBackgroundImage = [UIImage imageNamed:@"segmented_control-normal-bkgd.png"];
+        UIImage *normalBackgroundImage = [UIImage imageNamed:@"segmented_control-normal-bkgd"];
         [self setBackgroundImage:normalBackgroundImage
                         forState:UIControlStateNormal
                       barMetrics:UIBarMetricsDefault];
         
-        UIImage *selectedBackgroundImage = [UIImage imageNamed:@"segmented_control-selected-bkgd.png"];
+        UIImage *selectedBackgroundImage = [UIImage imageNamed:@"segmented_control-selected-bkgd"];
         [self setBackgroundImage:selectedBackgroundImage
                         forState:UIControlStateSelected
                       barMetrics:UIBarMetricsDefault];
@@ -55,10 +55,12 @@
                                                            forKey:UITextAttributeFont];
     [attributes setObject:[NSValue valueWithUIOffset:UIOffsetMake(0, 0)]
                    forKey:UITextAttributeTextShadowOffset];
+    [attributes setObject:[UIColor colorWithRed:115 green:181 blue:95 alpha:1]
+                   forKey:UITextAttributeTextColor];
     [self setTitleTextAttributes:attributes
                         forState:UIControlStateNormal];
     
-    [attributes setObject:UIColorFromRGB(0x73B55F)
+    [attributes setObject:[UIColor whiteColor]
                    forKey:UITextAttributeTextColor];
     [self setTitleTextAttributes:attributes
                         forState:UIControlStateSelected];
